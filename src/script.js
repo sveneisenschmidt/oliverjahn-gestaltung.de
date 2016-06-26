0eddc56fac1b0a043af2ff0ac218f2edc4cfecc5
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2016-06-26 20:11:09 +0200

    Slider verbessert

--- src/script.js
+++ src/script.js
@@ -7,12 +7,12 @@ require('../node_modules/bootstrap/dist/js/bootstrap.js');
 require('../src/nanoscroll.js');
 
 
-jQuery(document).ready(function() {
+jQuery(window).load(function() {
     jQuery(".nano").nanoScroller({ flash: true });
 });
 
 jQuery(document).ready(function() {
-    jQuery("#references a").on('click', function(event) {
+    jQuery("#references a").on('click', function(event) { 
         event.preventDefault();
         jQuery('#gallery img').attr('src', jQuery(this).attr('href')).on('load', function() {
             jQuery('#gallery').modal('show');
